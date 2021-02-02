30cde37f10236a1b799a2492643f5a3520bd6438
tribhuwanpandey <58321142+tribhuwanpandey@users.noreply.github.com>
2021-02-02 19:18:53 +0530

    Add files via upload

---
+++ springboot/springboot/src/main/java/com/aspirewebs/springboot/SpringbootCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.aspirewebs.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudAppApplication.class, args);
+	}
+
+}
---
+++ springboot/springboot/src/test/java/com/aspirewebs/springboot/SpringbootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirewebs.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
